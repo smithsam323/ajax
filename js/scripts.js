589e00be3cc5c0ae1afeb8643e1ce66575b0ebd9
smithsam323 <smithsam323@gmail.com>
2017-05-04 16:42:09 -0700

    Version 2
    * added Airtable content

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//INCLUDES
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 
@@ -7,4 +8,22 @@
 
 // $('#data').load('/data/data.html');
 
-$('#data').load('/data/data.json');
+//$('#data').load('/data/data.json');
+
+// Get from Airtable
+// Handshake with Airtable
+var Airtable = require('airtable');
+
+var base = Airtable.base('appCOnEuMBhfBiPry');
+$('#data').load('/data/airtable.js');
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyqJaj41LYH3j0kA'}).base('appCOnEuMBhfBiPry');
+
+base('Ajax Test').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+    if (err) { console.error(err); return; }
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Name'));
+    });
+});
