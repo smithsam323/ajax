c704872f2099f58de0e0edb0a58699e712571e74
smithsam323 <smithsam323@gmail.com>
2017-05-04 15:55:05 -0700

    Version 1
    *Intro to Ajax

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// @codekit-prepend "jquery.js";
+// @codekit-prepend "semantic.js";
+
+//'load()' method
+
+// $('#data').load('/data/data.txt');
+
+// $('#data').load('/data/data.html');
+
+$('#data').load('/data/data.json');
